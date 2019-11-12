7092fd567db035e07fb816a0cde98b4b4083e7f2
dengzq1234 <dengziqi1234@gmail.com>
2019-11-12 16:01:51 +0100

    changed layout of results page; changed font of results; add names of feature;

--- gmgc/static/gmgc/js/vue_components.js
+++ gmgc/static/gmgc/js/vue_components.js
@@ -2,20 +2,20 @@
 var cluster_features = {
     members: {
         cl: "Cluster ID",
-        clm: "Members",
-        al: "al",
-        hu: "hu",
-        avl: "avl",
-        hk: "hk",
-        avi: "avi",
-        mds: "mds",
-        nh: "ng",
-        lg: "lg",
-        naa: "naa",
-        mup: "mup",
-        mrp: "mrp",
-        sm: "sm",
-        nu: "nu"
+        clm: "Gene Members",
+        al: "Alignment length",
+        hu: "Number of genes with unknown function in pfam",
+        avl: "Average length of cluster members",
+        hk: "Number of genes with known function in pfam",
+        avi: "Average identity of cluster members",
+        mds: "Identity of the member more distant to the cluster",
+        nh: "Number of genes with no pfam hit in the cluster",
+        lg: "Largest sequence of a member",
+        naa: "Num of amino acids in cluster members",
+        mup: "Identity of most unrelated pair",
+        mrp: "Identity of most related pair",
+        sm: "Smallest sequence of a member",
+        nu: "Num cluster members"
         },
     paths:{
         psd: "psd"
@@ -110,52 +110,43 @@ var ClusterData = {
     template:`
 
   <div>
-    <div>
-        {{ cluster_features.members.cl }}
-        <div v-if="cluster_data.members">{{ cluster_data.members.cl }}</div>
-        <div v-else>No item</div>
+    <div id='main' class="container">
+        <h2>Cluster data</h2>
     </div>
-    
-    <div>
-      {{cluster_features.members.clm}}
-      <div id="example-1">
-        <div v-if="cluster_data.members">
-            <div v-if="cluster_data.members.clm">
-                <li v-for="member in cluster_data.members.clm">
-                    <a v-bind:href="'/gmgc/unigene/'+ member"> {{member}} </a>
-                </li>
-            </div>
-            <div v-else>
-                <a v-bind:href="'/gmgc/unigene/'+ cluster_data.members.cl"> {{cluster_data.members.cl}}</a>
-            </div>
+    <div class="annoBlock col">
+        <h3>Basic information</h3>
+        <font face="Arial">
+        <tr><th class="block">{{cluster_features.members.cl}}</th><td v-if="cluster_data.members">{{ cluster_data.members.cl }}</td><td v-else>No item</td></tr>
+        <tr><th class="block">Pathway</th><td v-if="cluster_data.paths">{{ cluster_data.paths.psd }}</td><td v-else>No item</td></tr>
+        <tr><th class="block">Suffixes</th><td v-if="cluster_data.suffixes">{{ cluster_data.suffixes.sfx }}</td><td v-else>No item</td></tr>
+        </font>
+        <div>
+             <tr><th><font face="Arial">Members</font></th></tr>
+                <div v-if="cluster_data.members">
+                    <div v-if="cluster_data.members.clm">
+                        <li v-for="member in cluster_data.members.clm">
+                            <tr><th class="block"></th><td class="block"><a v-bind:href="'/gmgc/unigene/'+ member"> {{member}} </a></td></tr>
+                        </li>
+                    </div>
+                    <div v-else>
+                        <tr><th class="block"></th><td class="block"><a v-bind:href="'/gmgc/unigene/'+ cluster_data.members.cl"> {{cluster_data.members.cl}}</a></td></tr>
+                    </div>
+                 </div>
+                 <tr v-else><th class="block"></th><td class="block">No item</td></tr>
         </div>
-        <li v-else>No item</li>
-      </div>
     </div>
-
-    <div> Members Annotation </div>
-    <div class="row">
-        
-        <li v-if="!cluster_data.members" class="block">No item</li>
-        <li v-else
-            v-for="(value, key) in cluster_data.members">
-            <div v-if=" key !== 'cl' & key !== 'clm' " class="block">{{ cluster_features.members[key]}}
-                <div v-if=" key !== 'cl' & key !== 'clm' " >{{ value }}</div>
+    
+    <div class="annoBlock col">
+        <h3>Functional Annoation</h3>
+            <div  v-if="cluster_data.members">
+                <font color="blue" style="border-bottom:solid 1px #000000;">Cluster Members Annotation</font>
             </div>
-        </li>
+            <li v-if="!cluster_data.members" class="block">No item</li>
+                <li v-else
+                    v-for="(value, key) in cluster_data.members">      
+                    <tr v-if=" key !== 'cl' & key !== 'clm'" ><th width="500px" height="30px" nowrap><font face="Arial">{{ cluster_features.members[key]}}</font></th><td class="block">{{ value }}</td></tr>
+                </li>
     </div>
-    
-    <div>
-        Pathway
-        <li v-if="cluster_data.paths">{{ cluster_data.paths.psd }}</li>
-        <div v-else>No item found</div>
-    </div>
-    
-    <div>
-        Suffiexes
-        <li v-if="cluster_data.suffixes">{{ cluster_data.suffixes.sfx }}</li>
-        <div v-else>No item found</div>
-    </div>   
   </div>
   
 `}
@@ -169,115 +160,122 @@ var UnigeneData = {
     props: ['csrf'],
     template:`
   <div>
-    <div>Unigene data</div>
-    <div>
-      Unigene
-      <div v-if="unigene.clusters.u">{{ unigene.clusters.u }}</div>
-      <div v-else>No item</div>
+    <div id='main' class="container">
+        <h2>Unigene data</h2>
     </div>
-    <div>
-      Cluster 
-      <div><a v-bind:href="'/gmgc/cluster/'+ unigene.clusters.cl"> {{ unigene.clusters.cl }} </a></div>
-    </div>
-    <div>
-      Sequence 
-      <div v-if='unigene.sequences.sq'>{{ unigene.sequences.sq }}</div>
-      <div v-else>No item</div>
-    </div>
-    <div>
-      Suffixes
-      <div v-if="unigene.suffixes.sfx">{{ unigene.suffixes.sfx }}</div>
-      <div v-else>No item</div>
-    </div>
-    <div  v-if="unigene.emapper_v2">
-      eggNOG_v2 Annotation 
-      <!--<div>{{ unigene.emapper_v2 }}</div>-->
-      <div>
-      <table rules="rows">
-          <tr><th class="block">p_n</th><td class="block">{{unigene.emapper_v2.p_n}}</td></tr> 
-          <tr><th class="block">s_o_s</th><td class="block">{{unigene.emapper_v2.s_o_s}}</td></tr>
-          <tr><th class="block">s_o_e</th><td class="block">{{unigene.emapper_v2.s_o_e}}</td></tr>
-          <tr><th class="block">COG</th><td class="block">{{unigene.emapper_v2.COG}}</td></tr>
-          <tr><th class="block">K_ko</th><td class="block">{{unigene.emapper_v2.K_ko}}</td></tr>
-          <tr><th class="block">b_tax_l</th><td class="block">{{unigene.emapper_v2.b_tax_l}}</td></tr>
-          <tr><th class="block">an_l_max</th><td class="block">{{unigene.emapper_v2.an_l_max}}</td></tr>
-          <tr><th class="block">K_ko</th><td class="block">{{unigene.emapper_v2.K_ko}}</td></tr>
-          <tr><th class="block">K_P</th><td class="block">{{unigene.emapper_v2.K_P}}</td></tr>
-          <tr><th class="block">K_R</th><td class="block">{{unigene.emapper_v2.K_R}}</td></tr>
-          <tr><th class="block">BiGG</th><td class="block">{{unigene.emapper_v2.BiGG}}</td></tr>
-          <tr><th class="block">K_M</th><td class="block">{{unigene.emapper_v2.K_M}}</td></tr>
-          <tr><th class="block">bOGs</th><td class="block">{{unigene.emapper_v2.bOGs}}</td></tr>
-          <tr><th class="block">s_e_o</th><td class="block">{{unigene.emapper_v2.s_e_o}}</td></tr>
-          <tr><th class="block">K_TC</th><td class="block">{{unigene.emapper_v2.K_TC}}</td></tr>
-          <tr><th class="block">OGs</th><td class="block">{{unigene.emapper_v2.OGs}}</td></tr>
-          <tr><th class="block">EC</th><td class="block">{{unigene.emapper_v2.EC}}</td></tr>
-          <tr><th class="block">ds</th><td class="block">{{unigene.emapper_v2.ds}}</td></tr>
-          <tr><th class="block">BRITE</th><td class="block">{{unigene.emapper_v2.BRITE}}</td></tr>
-          <tr><th class="block">CAZy</th><td class="block">{{unigene.emapper_v2.CAZy}}</td></tr>
-          <tr><th class="block">GOs</th><td class="block">{{unigene.emapper_v2.GOs}}</td></tr>
-          <tr><th class="block">K_rc</th><td class="block">{{unigene.emapper_v2.K_rc}}</td></tr>
-      </table>
-      </div><div v-else>No emapper hit</div> 
+    <div class="annoBlock col">
+        <h3>Basic information</h3>
+        <font face="Arial">
+        <tr><th class="block">Unigene</th><td v-if="unigene.clusters">{{ unigene.clusters.u }}</td><td v-else>No item</td></tr>
+        <tr><th class="block">Cluster</th><td v-if="unigene.clusters"><a v-bind:href="'/gmgc/cluster/'+ unigene.clusters.cl">{{ unigene.clusters.cl }}</a></td><td v-else>No item</td></tr>
+        <tr><th class="block">Sequence</th><td v-if="unigene.sequences">{{ unigene.sequences.sq }} </td><td v-else>No item</td></tr>
+        <tr><th class="block">Suffixes</th><td v-if="unigene.suffixes">{{ unigene.suffixes.sfx }}</td><td v-else>No item</td></tr>
+        </font>
     </div>
     
-    <div v-if="unigene.pfam">
-      Pfam domains
-      <!--<div>{{ unigene.pfam.pf }}</div>-->
-      <div>
-      <li v-for="object in unigene.pfam.pf">
-            <tr><th class="block">n</th><td class="block">{{ object.n }}</td></tr>
-            <tr><th class="block">sc</th><td class="block">{{ object.sc }}</td></tr>
-            <tr><th class="block">s</th><td class="block">{{ object.s }}</td></tr>
-            <tr><th class="block">ev</th><td class="block">{{ object.ev }}</td></tr>
-            <tr><th class="block">e</th><td class="block">{{ object.e }}</td></tr>
-      </li>
-      </div>
-    </div><div v-else>No Pfam hit</div>
+    <div class="annoBlock col">
+    <h3>Functional annotation</h3>
+        <div  v-if="unigene.emapper_v2">
+          <font color="blue">eggNOG_v2 Annotation</font>
+          <!--<div>{{ unigene.emapper_v2 }}</div>-->
+          <div>
+          <table rules="rows " style="border-top:solid 1px #000000;">
+              <font face="Arial">
+              <tr><th width="225px" height="30px" nowrap>Preferred_name</th><td class="block">{{unigene.emapper_v2.p_n}}</td></tr> 
+              <tr><th width="225px" height="30px" nowrap>Seed_ortholog_score</th><td class="block">{{unigene.emapper_v2.s_o_s}}</td></tr>
+              <tr><th width="225px" height="30px" nowrap>Seed_ortholog_evalue</th><td class="block">{{unigene.emapper_v2.s_o_e}}</td></tr>
+              <tr><th width="225px" height="30px" nowrap>seed_eggNOG_ortholog</th><td class="block">{{unigene.emapper_v2.s_e_o}}</td></tr>
+              <tr><th width="225px" height="30px" nowrap>COG</th><td class="block">{{unigene.emapper_v2.COG}}</td></tr>
+              <tr><th width="225px" height="30px" nowrap>KEGG_ko</th><td class="block">{{unigene.emapper_v2.K_ko}}</td></tr>
+              <tr><th width="225px" height="30px" nowrap>Best_tax_level</th><td class="block">{{unigene.emapper_v2.b_tax_l}}</td></tr>
+              <tr><th width="225px" height="30px" nowrap>Annot_level_max</th><td class="block">{{unigene.emapper_v2.an_l_max}}</td></tr>
+              <tr><th width="225px" height="30px" nowrap>KEGG_Pathway</th><td class="block">{{unigene.emapper_v2.K_P}}</td></tr>
+              <tr><th width="225px" height="30px" nowrap>KEGG_Reaction</th><td class="block">{{unigene.emapper_v2.K_R}}</td></tr>
+              <tr><th width="225px" height="30px" nowrap>KEGG_rclass</th><td class="block">{{unigene.emapper_v2.K_rc}}</td></tr>
+              <tr><th width="225px" height="30px" nowrap>BiGG_Reaction</th><td class="block">{{unigene.emapper_v2.BiGG}}</td></tr>
+              <tr><th width="225px" height="30px" nowrap>KEGG_Module</th><td class="block">{{unigene.emapper_v2.K_M}}</td></tr>
+              <tr><th width="225px" height="30px" nowrap>bestOG</th><td class="block">{{unigene.emapper_v2.bOGs}}</td></tr>
+              <tr><th width="225px" height="30px" nowrap>KEGG_TC</th><td class="block">{{unigene.emapper_v2.K_TC}}</td></tr>
+              <tr><th width="225px" height="30px" nowrap>matching_OGs</th><td class="block">{{unigene.emapper_v2.OGs}}</td></tr>
+              <tr><th width="225px" height="30px" nowrap>EC</th><td class="block">{{unigene.emapper_v2.EC}}</td></tr>
+              <tr><th width="225px" height="30px" nowrap>Description</th><td>{{unigene.emapper_v2.ds}}</td></tr>
+              <tr><th width="225px" height="30px" nowrap>BRITE</th><td class="block">{{unigene.emapper_v2.BRITE}}</td></tr>
+              <tr><th width="225px" height="30px" nowrap>CAZy</th><td class="block">{{unigene.emapper_v2.CAZy}}</td></tr>
+              <tr><th width="225px" height="30px" nowrap>GOs</th><td class="block">{{unigene.emapper_v2.GOs}}</td></tr>
+              </font>
+          </table>
+          </div><div v-else>No emapper hit</div> 
+        </div>
     
-    <div v-if="unigene.sprot_best">
-      SwissProt best hit 
-      <!-- <div>{{ unigene.sprot_best.spb }}</div> -->
-      <div> 
-        <tr><th class="block">n</th><td class="block">{{ unigene.sprot_best.spb.n }}</td></tr>
-        <tr><th class="block">ev</th><td class="block">{{ unigene.sprot_best.spb.ev }}</td></tr>
-        <tr><th class="block">qc</th><td class="block">{{ unigene.sprot_best.spb.qc }}</td></tr>
-        <tr><th class="block">sc</th><td class="block">{{ unigene.sprot_best.spb.sc }}</td></tr>
-        <tr><th class="block">pi</th><td class="block">{{ unigene.sprot_best.spb.pi }}</td></tr>
-        <tr><th class="block">tc</th><td class="block">{{ unigene.sprot_best.spb.tc }}</td></tr>
-        <tr><th class="block">exact hit</th><td v-if="unigene.sprot_exact" class="block">{{ unigene.sprot_exact.spe }}</td><td v-else class="block">no exact hit</td></tr>
-      </div>
-    </div><div v-else>No SwissProt hit</div>
+        <div v-if="unigene.pfam">
+          <font color="blue">Pfam annotation</font>
+          <!--<div>{{ unigene.pfam.pf }}</div>-->
+          <div>
+          <li v-for="object in unigene.pfam.pf">
+                <table rules="rows " style="border-top:solid 1px #000000;">
+                <font face="Arial">
+                <tr><th width="225px" height="30px" nowrap>Domain</th><td>{{ object.n }}</td></tr>
+                <tr><th width="225px" height="30px" nowrap>Start</th><td>{{ object.s }}</td></tr>
+                <tr><th width="225px" height="30px" nowrap>End</th><td>{{ object.e }}</td></tr>
+                <tr><th width="225px" height="30px" nowrap>E-value</th><td>{{ object.ev }}</td></tr>
+                <tr><th width="225px" height="30px" nowrap>Hit score</th><td>{{ object.sc }}</td></tr>
+                </font>
+                <table>
+          </li>
+          </div>
+        </div><div v-else><font color="blue">No Pfam hit</font></div>
     
-    <!--
-    <div>
-      SwissProt exact hit 
-      <div>{{ unigene.sprot_exact.spe }}</div>   
+        <div v-if="unigene.sprot_best">
+          <font color="blue">SwissProt best hit</font>
+          <!-- <div>{{ unigene.sprot_best.spb }}</div> -->
+          <div> 
+            <table rules="rows " style="border-top:solid 1px #000000;">
+            <font face="Arial">
+            <tr><th width="225px" height="30px" nowra>AC</th><td>{{ unigene.sprot_best.spb.n }}</td></tr>
+            <tr><th width="225px" height="30px" nowra>Query coverage</th><td>{{ unigene.sprot_best.spb.qc }}</td></tr>
+            <tr><th width="225px" height="30px" nowra>Target coverage</th><td>{{ unigene.sprot_best.spb.tc }}</td></tr>
+            <tr><th width="225px" height="30px" nowra>Score</th><td>{{ unigene.sprot_best.spb.sc }}</td></tr>
+            <tr><th width="225px" height="30px" nowra>E-value</th><td>{{ unigene.sprot_best.spb.ev }}</td></tr>
+            <tr><th width="225px" height="30px" nowra>Percent identity</th><td>{{ unigene.sprot_best.spb.pi }}</td></tr>
+            <tr><th width="225px" height="30px" nowra>Exact hit</th><td v-if="unigene.sprot_exact">{{ unigene.sprot_exact.spe }}</td><td v-else>no exact hit</td></tr>
+            </font>
+            <table>
+          </div>
+        </div><div v-else><font color="blue">No SwissProt hit</font></div>
+    
+        <div v-if="unigene.trembl_best">
+          <font color="blue">Trembl best hit</font>
+          <!-- <div>{{ unigene.trembl_best }}</div> -->
+          <div>
+            <table style="border-top:solid 1px #000000;">   
+            <font face="Arial">
+            <tr><th width="225px" height="30px" nowra>ID</th><td>{{ unigene.trembl_best.trb.n }}</td></tr>
+            <tr><th width="225px" height="30px" nowra>Query coverage</th><td>{{ unigene.trembl_best.trb.qc }}</td></tr>
+            <tr><th width="225px" height="30px" nowra>Target coverage</th><td>{{ unigene.trembl_best.trb.tc }}</td></tr>
+            <tr><th width="225px" height="30px" nowra>Score</th><td>{{ unigene.trembl_best.trb.sc }}</td></tr>
+            <tr><th width="225px" height="30px" nowra>E-value</th><td>{{ unigene.trembl_best.trb.ev }}</td>
+            <tr><th width="225px" height="30px" nowra>Percent identity</th><td>{{ unigene.trembl_best.trb.pi }}</td></tr>
+            </font>
+            <table>
+          </div>
+        </div><div v-else><font color="blue">No Trembl hit</font></div>
+    
+    
+        <div  v-if="unigene.neighbour">
+          <font color="blue">Neighbourhood</font>
+          <!--<div>{{ unigene.neighbour }}</div>-->
+          <div>
+          <table rules="rows " style="border-bottom:solid 1px #000000;">
+            <tr><th width="300px" height="30px" nowrap> orf_name </th><td class="block">start</td><td class="block">end</td><td class="block">strand</td></tr>
+          </table>
+          <li v-for="object in unigene.neighbour.o">
+            <font face="Arial">
+            <tr><th width="300px" height="30px" nowrap>{{object.g}}</th><td class="block">{{object.s[0]}}</td><td class="block">{{object.s[1]}}</td><td class="block">{{object.s[2]}}</td></tr>
+            </font>
+          </li>
+          </div>
+        </div><div v-else><font color="blue">No Neighbourhood hit</font></div>
     </div>
-    -->
-    <div v-if="unigene.trembl_best">
-      Trembl best hit
-      <!-- <div>{{ unigene.trembl_best }}</div> -->
-      <div>   
-        <tr><th class="block">n</th><td class="block">{{ unigene.trembl_best.trb.n }}</td></tr>
-        <tr><th class="block">ev</th><td class="block">{{ unigene.trembl_best.trb.ev }}</td>
-        <tr><th class="block">qc</th><td class="block">{{ unigene.trembl_best.trb.qc }}</td></tr>
-        <tr><th class="block">sc</th><td class="block">{{ unigene.trembl_best.trb.sc }}</td></tr>
-        <tr><th class="block">pi</th><td class="block">{{ unigene.trembl_best.trb.pi }}</td></tr>
-        <tr><th class="block">tc</th><td class="block">{{ unigene.trembl_best.trb.tc }}</td></tr>
-      </div>
-    </div><div v-else>No Trembl hit</div>
-    
-    
-    <div  v-if="unigene.neighbour">
-      Neighbourhood
-      <!--<div>{{ unigene.neighbour }}</div>-->
-      <div>
-      <tr><td class="block">start</td><td class="block">end</td><td class="block">strand</td><th class="block" width="pixels | %"> orf_name </th></tr>
-      <li v-for="object in unigene.neighbour.o">
-        <tr><td class="block">{{object.s[0]}}</td><td class="block">{{object.s[1]}}</td><td class="block">{{object.s[2]}}</td><th class="block" width="pixels | %">{{object.g}}</th></tr>
-      </li>
-      </div>
-    </div><div v-else>No Neighbourhood hit</div>
   </div>
 `}
 
